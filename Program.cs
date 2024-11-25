b3fca77416d85fb89fecaa94b1ba7ecb4f0cd59b
md ali reza <muhammd.ali@brainstation-23.com>
2024-11-26 01:33:19 +0600

    disabled automatic model validation response

--- Program.cs
+++ Program.cs
@@ -2,7 +2,11 @@ var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-builder.Services.AddControllers();
+
+builder.Services.AddControllers().ConfigureApiBehaviorOptions(options => {
+    options.SuppressModelStateInvalidFilter = true;
+});
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
